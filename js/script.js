4c1acdf0985db5d83d699b4e07387b0257e6b48f
SonyaCore <sonyacore@protonmail.com>
2022-10-29 16:49:47 +0330

    added time to js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-var span = document.getElementById('time');
+// show time 
+var spantime = document.getElementById('time');
 
 function time() {
   var d = new Date();
   var s = d.getSeconds();
   var m = d.getMinutes();
   var h = d.getHours();
-  span.textContent = 
+  spantime.textContent = 
     ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
 }
-
 setInterval(time, 1000);
 
+// show date 
+var spandate = document.getElementById('date');
+
+const date = new Date().toDateString();
+spandate.textContent = date;
 
 //// prevent enter for forms
 
